5726e090de692ee656bbd4b4c02647b66a519d17
Andrew Herubin <anderwherubin@outlook.com>
2022-11-26 10:15:59 -0600

    fix errors

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const app = Express()
 const port = process.env.PORT || 3333
 
 const siteRouter = require('./routes/site')
+const schemeRouter = require('./routes/scheme')
 
 app.use('/site', siteRouter)
+app.use('scheme', schemeRouter)
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}...`)
