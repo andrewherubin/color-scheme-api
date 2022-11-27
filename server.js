266505efb0820717d269f5ce773567a0ffa87ffc
Andrew Herubin <anderwherubin@outlook.com>
2022-11-26 23:21:32 -0600

    added cors

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const Express = require('express')
+const Cors = require('cors')
 
 const app = Express()
 const port = process.env.PORT || 3333
 
+const cors = Cors()
 const siteRouter = require('./routes/site')
 const schemeRouter = require('./routes/scheme')
 
+app.use(cors)
 app.use('/site', siteRouter)
 app.use('/scheme', schemeRouter)
 
